3d6d94549888eefa669b34c8759a952905787d58
Gustavo <tatacarrera@gmail.com>
2015-07-15 20:35:28 -0300

    more

--- Main.java
+++ Main.java
@@ -13,12 +13,17 @@ public class Main {
 	  	list.add(3);
 	  	list.add(5);
 
+	  	String a = "Hi there";
+	  	System.out.println(a.length());
+
 	  	System.out.println("size: " + list.size());
 
 	  	System.out.println("pos(0) " + list.get(0));
 	  	System.out.println("pos(1) " + list.get(1));
 	  	System.out.println("pos(2) " + list.get(2));
 	  	System.out.println("pos(4) " + list.get(4));
+
+
 
 	}
 }
