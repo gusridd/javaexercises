241c89b7839a51c34354724bc07f2bb84d75e119
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2015-07-28 10:23:55 -0300

    change to robot

--- Robot/Main.java
+++ Robot/Main.java
@@ -1,12 +1,12 @@
 public class Main {
 
   static public void main(String args[]){
-    IRobot robot = new RandomRobot();
-    moveTwiceRight(robot);
-    System.out.println("Robot final pos: " + robot.getPos());
+    //IRobot robot = new RandomRobot();
+    //moveTwiceRight(robot);
+    //System.out.println("Robot final pos: " + robot.getPos());
 
-    IRobot robot2 = new RandomRobot(0.3);
-    moveTwiceRight(robot2);
+    IRobot robot2 = new RandomRobot(0.1);
+    moveRightWithTrampoline(robot2).execute();
     System.out.println("Robot final pos: " + robot2.getPos());
   }
 
@@ -23,4 +23,36 @@ public class Main {
     }    
   }
 
+  static public Trampoline<Object> moveRightWithTrampoline(IRobot robot){
+    if(robot.move()) {
+      return new Trampoline<Object>() {
+        public Object get() { return new Object(); }
+      };
+    } else {
+      return new Trampoline<Object>() {
+        public Trampoline<Object> run() {
+          moveRightWithTrampoline(robot).execute();
+          return moveRightWithTrampoline(robot);
+        }
+      };
+    }
+  }
+
+}
+
+
+class Trampoline<T> 
+{
+    public T get() { return null; }
+    public Trampoline<T>  run() { return null; }
+
+    T execute() {
+        Trampoline<T>  trampoline = this;
+
+        while (trampoline.get() == null) {
+            trampoline = trampoline.run();
+        }
+
+        return trampoline.get();
+    }
 }
